643e028d46bab88a692b84800a8dc1599ce22f74
Iftekhar-13 <shams241-15-591@diu.edu.bd>
2025-05-12 20:02:57 +0600

    2nd java

---
+++ prog4.java
@@ -0,0 +1,14 @@
+
+    public class prog4 {
+        public static void main(String[] args)
+        {
+            System.out.print(8+7);
+            System.out.println(9-3);
+            System.out.println(8 * 7);
+            System.out.println(8 / 7);  
+            System.out.println(8 % 7);
+            System.out.println(8.0 / 7.0);
+            
+        }
+    }
+
