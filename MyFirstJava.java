2e5941dea8a74f8685775e5c2ae53c1d05be482d
Iftekhar-13 <shams241-15-591@diu.edu.bd>
2025-05-12 19:58:38 +0600

    java

---
+++ MyFirstJava.java
@@ -0,0 +1,9 @@
+public class MyFirstJava {
+    public static void main(String[] args) {
+        System.out.print("First output");
+        System.out.println("Second output");
+        double val = 5.34823;
+        System.out.printf("Formatted value = %.2f\n", val);
+        System.out.println("Last line");
+    }
+}
